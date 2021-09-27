4d5dc48b590b1daf6720132dc60f0e9927425f0f
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-27 15:33:43 +0500

    add if else in ejs

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
+  var today = new Date();
+  currentDay = today.getDay();
+  var day = "";
 
-    var today = new Date();
-    currentDay = today.getDay()
-    console.log(currentDay);
-    var day = ""
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+        console.log("Error: the current day is equal to: " + currentDay);
+  }
+  res.render("list", { kindOfDay: day });
+});
 
-    switch (currentDay) {
-        
-    }
-
-    if (today.getDay >= 5) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
-    res.render("list", {kindOfDay: day});
-})
-
-app.listen(3000, function() {
-    console.log("Server is running on port 3000...")
-})
+app.listen(3000, function () {
+  console.log("Server is running on port 3000...");
+});
