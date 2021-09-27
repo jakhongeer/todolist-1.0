58235ffd02c1d14758c384e03b6a18b15ebf54df
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-27 16:34:41 +0500

    add for loop to add more <li> in list.ejs

--- app.js
+++ app.js
@@ -3,40 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
+
+var item = "";
+var items = [];
 
 app.get("/", function (req, res) {
   var today = new Date();
   currentDay = today.getDay();
   var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-        console.log("Error: the current day is equal to: " + currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+  var options = {
+      weekday : "long",
+      day : "numeric",
+      month : "long",
+  };
+  
+  day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+
+    items.push(item); 
+    res.redirect("/")
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000...");
