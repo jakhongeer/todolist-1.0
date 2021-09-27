4f02a2474e6a9b8f1a65413cc4e25693bc85814f
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-27 17:59:30 +0500

    add work route but with bugs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static("public"))
 
 var item = "";
 var items = ["Get up at 7 am", "Go to uni", "Go to home"];
+var workItems = [];
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -23,15 +24,26 @@ app.get("/", function (req, res) {
   };
   
   day = today.toLocaleDateString("en-US", options);
-  res.render("list", { listTitle: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
     item = req.body.newItem;
-
-    items.push(item); 
-    res.redirect("/")
+    
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/")
+    }
 })
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000...");
